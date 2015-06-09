b88cc1db6e1ea89080b7f0b5f1a624428e88481f
Dwayne Bent <dbb@dbb.io>
2015-06-09 18:22:42 -0400

    Seperate behaviors

--- Source/HotSpot/HotSpotBehavior.cs
+++ Source/HotSpot/HotSpotBehavior.cs
@@ -7,63 +7,28 @@ using UnityEngine;
 namespace HotSpot
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
-    public class HotSpotBehaviour : MonoBehaviour
+    public class HotSpotBehavior : MonoBehaviour
     {
-        private bool _lastThermalColorsDebug;
-        private readonly ScreenMessage _screenMessage =
-            new ScreenMessage(string.Empty, 4, ScreenMessageStyle.LOWER_CENTER);
-
         #region MonoBehaviour
 
         public void LateUpdate()
         {
-            Log.Trace("Entering HotSpotBehaviour.LateUpdate()");
+            Log.Trace("Entering HotSpotBehavior.LateUpdate()");
 
             if (Config.Instance.Overlay.Enable)
             {
-                var metric = Config.Instance.Overlay.Metric;
-
-                LateUpdateScreenMessage(metric);
-                LateUpdateColor(metric);
+                LateUpdateColor();
             }
 
-            Log.Trace("Leaving HotSpotBehaviour.LateUpdate()");
+            Log.Trace("Leaving HotSpotBehavior.LateUpdate()");
         }
 
-        private void LateUpdateScreenMessage(Metric metric)
-        {
-            if (_lastThermalColorsDebug != PhysicsGlobals.ThermalColorsDebug)
-            {
-                Log.Debug("PhysicsGlobals.ThermalColorsDebug has been toggled");
-
-                var flightOverlays = FindObjectOfType<FlightOverlays>();
-
-                if (flightOverlays != null)
-                {
-                    Log.Debug("Found FlightOverlays, removing its ScreenMessage");
-
-                    ScreenMessages.RemoveMessage(flightOverlays.TryGetScreenMessage());
-                }
-
-                if (Config.Instance.Overlay.EnableScreenMessage)
-                {
-                    var scheme = Config.Instance.Overlay.GetActiveMetric().GetActiveScheme();
-
-                    var metricMsg = metric.FriendlyName;
-                    var schemMsg = scheme.FriendlyName == null ? string.Empty : $" ({scheme.FriendlyName})";
-                    var stateMsg = PhysicsGlobals.ThermalColorsDebug ? "Enabled" : "Disabled";
-
-                    ScreenMessages.PostScreenMessage($"{metricMsg}{schemMsg} Overlay: {stateMsg}", _screenMessage);
-                }
-            }
-
-            _lastThermalColorsDebug = PhysicsGlobals.ThermalColorsDebug;
-        }
-
-        private static void LateUpdateColor(Metric metric)
+        private static void LateUpdateColor()
         {
             if (PhysicsGlobals.ThermalColorsDebug)
             {
+                var metric = Config.Instance.Overlay.Metric;
+
                 foreach (var vessel in FlightGlobals.Vessels.Where(i => i.loaded))
                 {
                     var vesselVariables = metric.GetVesselValues(vessel);
---
+++ Source/HotSpot/HotSpotGuiBehavior.cs
@@ -0,0 +1,59 @@
+﻿using HotSpot.Reflection;
+using UnityEngine;
+
+namespace HotSpot
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public sealed class HotSpotGuiBehavior : MonoBehaviour
+    {
+        private bool _lastThermalColorsDebug;
+        private readonly ScreenMessage _screenMessage =
+            new ScreenMessage(string.Empty, 4, ScreenMessageStyle.LOWER_CENTER);
+
+        #region MonoBehaiour
+
+        public void LateUpdate()
+        {
+            Log.Trace("Entering HotSpotGuiBehavior.LateUpdate()");
+
+            if (Config.Instance.Overlay.Enable)
+            {
+                LateUpdateScreenMessage();
+            }
+
+            Log.Trace("Leaving HotSpotGuiBehavior.LateUpdate()");
+        }
+
+        private void LateUpdateScreenMessage()
+        {
+            if (_lastThermalColorsDebug != PhysicsGlobals.ThermalColorsDebug)
+            {
+                Log.Debug("PhysicsGlobals.ThermalColorsDebug has been toggled");
+
+                var flightOverlays = FindObjectOfType<FlightOverlays>();
+
+                if (flightOverlays != null)
+                {
+                    Log.Debug("Found FlightOverlays, removing its ScreenMessage");
+
+                    ScreenMessages.RemoveMessage(flightOverlays.TryGetScreenMessage());
+                }
+
+                if (Config.Instance.Overlay.EnableScreenMessage)
+                {
+                    var scheme = Config.Instance.Overlay.GetActiveMetric().GetActiveScheme();
+
+                    var metricMsg = Config.Instance.Overlay.Metric.FriendlyName;
+                    var schemMsg = scheme.FriendlyName == null ? string.Empty : $" ({scheme.FriendlyName})";
+                    var stateMsg = PhysicsGlobals.ThermalColorsDebug ? "Enabled" : "Disabled";
+
+                    ScreenMessages.PostScreenMessage($"{metricMsg}{schemMsg} Overlay: {stateMsg}", _screenMessage);
+                }
+            }
+
+            _lastThermalColorsDebug = PhysicsGlobals.ThermalColorsDebug;
+        }
+
+        #endregion
+    }
+}
