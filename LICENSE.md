217c38f686c961f204495086d28b248149c9a8b0
Dwayne Bent <dbb@dbb.io>
2016-05-14 10:02:43 -0400

    Build Overhaul

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 This software is distributed under the MIT license.
 
-    Copyright (c) 2015 Dwayne Bent
+    Copyright (c) 2015-2016 Dwayne Bent
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Hot Spot")]
+[assembly: AssemblyCompany("Hot Spot Contributors")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
